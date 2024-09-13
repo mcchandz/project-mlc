7b9bca480e889313c5536bfcf55d9b820b732ae3
mcchandz <chandrashekarchandana4@gmail.com>
2024-09-13 16:48:09 +0530

    successfully pusheed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const mongo = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
+
 require('dotenv').config({path:'./config/dev.env'});
 require("./db/mongoose");
 const {router} = require('./router/user.router');
@@ -10,6 +11,7 @@ app.use(bodyParser.json());
 app.use(router);
 
 const port = process.env.PORT;
+
 app.listen(port, () => {
     console.log(`Server running at ${port}`);
 })
